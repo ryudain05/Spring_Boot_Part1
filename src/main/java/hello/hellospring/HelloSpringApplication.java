2d27bad7533e4c4a8d67751622f6889b25746255
RDI <dkdlsekdls@naver.com>
2023-03-13 13:49:02 +0900

    feat/홈화면 추가

--- src/main/java/hello/hellospring/HelloSpringApplication.java
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -1,4 +1,4 @@
-package hello.hellospring.service;
+package hello.hellospring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HelloSpringApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(HelloSpringApplication.class, args);
 	}
 
--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,8 +1,9 @@
-package hello.hellospring.service;
+package hello.hellospring;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
