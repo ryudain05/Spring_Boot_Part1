3efcde85a2bc3e9edf6218c458079ec351424fa9
RDI <dkdlsekdls@naver.com>
2023-03-31 23:11:14 +0900

    feat/새로운 할인 정책 개발

---
+++ core/src/main/java/hello/core/discount/ReteDiscountPolicy.java
@@ -0,0 +1,16 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class ReteDiscountPolicy implements DiscountPolicy{
+    private int discountPolicy = 10;
+    @Override
+    public int discount(Member member, int price) {
+        if(member.getGrade()== Grade.VIP){
+            return price * discountPolicy/100;
+        }else{
+            return 0;
+        }
+    }
+}
