d85b0cc01c42d84c89d0488638f2f350f315503e
ryudain05 <dkdlsekdls@naver.com>
2023-04-28 15:03:50 +0900

    feat/생성자 주입 선택

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -12,30 +12,15 @@ import java.util.concurrent.ThreadPoolExecutor;
 public class OrderServiceImpl implements OrderService{
 
     //불변의 특징을 가져야한다. (불변 객체)
-    private MemberRepository memberRepository;
-    private DiscountPolicy discountPolicy;
-
-//    public void setMemberRepository(MemberRepository memberRepository) {
-//        System.out.println("memberRepository = " + memberRepository);
-//        this.memberRepository = memberRepository;
-//    }
-//
-//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
-//        System.out.println("discountPolicy = " + discountPolicy);
-//        this.discountPolicy = discountPolicy;
-//    }
+    //final로 생성자에서만 값을 넣어주기
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
 
 
-//    //Autowired가 있으면 스프링 컨테이너에서 꺼내고 주입한다.
-//    //생성자가 하나일땐 Autowired 설정을 해주지 않아도 된다.
+    //Autowired가 있으면 스프링 컨테이너에서 꺼내고 주입한다.
+    //생성자가 하나일땐 Autowired 설정을 해주지 않아도 된다.
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         System.out.println("1. OrderServiceImpl.OrderServiceImpl ");
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
-
-    @Autowired
-    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy){
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
