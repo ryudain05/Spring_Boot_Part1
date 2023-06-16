db22c58b5e9ec1863fee1ad97c24fbd76d3bca6a
ryudain05 <dkdlsekdls@naver.com>
2023-06-16 13:46:50 +0900

    feat/스프링MVC 시작

---
+++ servlet/src/main/java/hello/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
@@ -0,0 +1,17 @@
+package hello.servlet.web.springmvc.v1;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+//@Component
+//class레벨에 있어야 함. 메소드레벨에 있으면 작동X
+@RequestMapping
+public class SpringMemberFormControllerV1 {
+    @RequestMapping("/springmvc/v1/members/new-form")
+    public ModelAndView process() {
+        return new ModelAndView("new-form");
+    }
+}
