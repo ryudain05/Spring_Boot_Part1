e95db00ccf8174a54636ff837bb6551707de9012
ryudain05 <dkdlsekdls@naver.com>
2023-06-18 21:25:17 +0900

    feat/MVC - 실용적인 방식

--- servlet/src/main/java/hello/servlet/web/springmvc/v2/SpringMemberControllerV2.java
+++ servlet/src/main/java/hello/servlet/web/springmvc/v2/SpringMemberControllerV2.java
@@ -24,10 +24,15 @@ public class SpringMemberControllerV2 {
 
 
     @RequestMapping("/save")
-    public ModelAndView save() {
-        List<Member> members = memberRepository.findAll();
-        ModelAndView mv = new ModelAndView("members");
-        mv.addObject("members", members);
+    public ModelAndView save(HttpServletRequest request, HttpServletResponse response) {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        ModelAndView mv = new ModelAndView("save-result");
+        mv.addObject("member", member);
         return mv;
     }
 
