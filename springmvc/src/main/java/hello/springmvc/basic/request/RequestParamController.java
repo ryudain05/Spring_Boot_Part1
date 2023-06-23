2963de869b97ea3fca4c849b59bbb8e4eb9fac1e
ryudain05 <dkdlsekdls@naver.com>
2023-06-23 19:48:09 +0900

    feat/HTTP 요청파라미터 - @RequestParam

--- springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -5,7 +5,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
 
@@ -21,5 +24,31 @@ public class RequestParamController {
         log.info("username = {} , age = {}", username, age);
 
         response.getWriter().write("ok");
+    }
+
+    //Controller면서 String이면 View를 찾게 됨으로 ResponseBody로 반환한다.
+    @ResponseBody
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(
+            @RequestParam ("username")String memberName,
+            @RequestParam ("age") int memberAge)
+            {
+                log.info("username = {} , age = {}", memberName, memberAge);
+                return "ok";
+            }
+    @ResponseBody
+    @RequestMapping("/request-param-v3")
+    public String requestParamV3(
+            @RequestParam String username,
+            @RequestParam int age)
+    {
+        log.info("username = {} , age = {}", username, age);
+        return "ok";
+    }
+    @ResponseBody
+    @RequestMapping("/request-param-v4")
+    public String requestParamV4(String username, int age) {
+        log.info("username = {} , age = {}", username, age);
+        return "ok";
     }
 }
